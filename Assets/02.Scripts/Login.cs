f60999c116fc75632543dc8158a8cb3643665c2c
yjyj713 <yjyj713@naver.com>
2025-03-25 14:36:38 +0900

    <Feat> : "로그인 창 UI 완"

---
+++ Assets/02.Scripts/Login.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Login : MonoBehaviour
+{
+    GameManager gameManager;
+    UserData userData;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
